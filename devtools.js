08f91c61a458c271581bd678c8d9212a2c0b3884
Gj <gongjie0422@163.com>
2021-06-03 13:11:52 +0800

    :tada: init;

---
+++ devtools.js
@@ -0,0 +1,9 @@
+// 创建一个Panel
+chrome.devtools.panels.create(
+  // title
+  "AjaxC",
+  // iconPath
+  null,
+  // pagePath
+  "panel.html"
+);
---
+++ panel.js
@@ -0,0 +1,31 @@
+// Chrome DevTools Extension中不能使用console.log
+const log = (...args) =>
+  chrome.devtools.inspectedWindow.eval(`
+    console.log(...${JSON.stringify(args)});
+`);
+
+// 注册回调，每一个http请求响应后，都触发该回调
+chrome.devtools.network.onRequestFinished.addListener(async (...args) => {
+  try {
+    const [
+      {
+        // 请求的类型，查询参数，以及url
+        request: { method, queryString, url },
+
+        // 该方法可用于获取响应体
+        getContent,
+      },
+    ] = args;
+
+    log('[--ajax--]', method, queryString, url);
+
+    // 将callback转为await promise
+    // warn: content在getContent回调函数中，而不是getContent的返回值
+    const content = await new Promise((res, rej) => getContent(res));
+    log('[--ajax--]', content);
+  } catch (err) {
+    log('[--ajax--]', err.stack || err.toString());
+  }
+});
+
+log('[--ajax--]', '开始记录--------')
